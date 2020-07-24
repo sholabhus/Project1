6df95d59c9da85fdc5c5b48ea84e86dec7256676
Shola Abhulimhen <Sholaabhus@gmail.com>
2020-07-24 23:23:09 +0100

    Detail - Version1

---
+++ src/Hollow.java
@@ -0,0 +1,15 @@
+//Program to output a hollow 5 * 5 square or asterisk
+public class Hollow {
+
+    public static void main(String[] args) {
+        {
+            System.out.println(" * * * *   *");
+            System.out.println(" *         *");
+            System.out.println(" *         *");
+            System.out.println(" *         *");
+            System.out.println(" *  * * *  *");
+        }
+
+    }
+}
+
