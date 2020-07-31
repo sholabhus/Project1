5a9f69da9b7e04828b9271586f45abcc7f0b375a
Shola Abhulimhen <Sholaabhus@gmail.com>
2020-07-31 20:28:24 +0100

    Detail - Version1

---
+++ src/Convert.java
@@ -0,0 +1,20 @@
+public class Convert {
+
+    public static void main (String[]args) {
+
+        int k = 5;
+        switch(k) {
+            case 1 ://case value 1
+            System.out.println("Edit");
+            break;
+            case 2://case value 2
+                System.out.println("Add");
+                break;
+            case 3://case value  3
+                System.out.println("Quilt");
+                break;
+            default:System.out.println("Invalid");
+
+        }
+    }
+}
---
+++ src/Exercise.java
@@ -0,0 +1,11 @@
+ public class Exercise {
+
+     //Main method
+    public static void main(String[]args) {
+
+        //variables
+        double a =20,b =30,answer;
+        answer = a + b;
+        System.out.println("Sum is " + answer);
+    }
+}
