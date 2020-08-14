1058c8e66d70da65f538ddc4e6cb0f894fe6227d
Shola Abhulimhen <Sholaabhus@gmail.com>
2020-08-14 17:44:58 +0100

    Detail - Version1

---
+++ src/calculate.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class calculate {
+
+    public static void main (String[]args) {
+
+
+        Scanner scan = new Scanner(System.in);  //create scanner object to get user input
+        System.out.print("Enter width of the windows in meters :");// Output user input
+        double width = scan.nextDouble(); // reads a double value from the user
+
+
+        System.out.print("Enter height of the windows in meters:"); //output user input
+        double height = scan.nextDouble();//reads a double value from the user
+
+        double area = width * height;// To find area
+        double perimeter = width + width + height + height;// find perimeter
+
+        if (width < 0.5 || width > 3.5) {
+            System.out.println("Window width must be between 0.5 and 3.5");
+        }
+
+        if (height < 0.5 || height > 2.0) {
+            System.out.println("Window height must be between 0.5 and 2.0");
+        }
+
+        double feet = perimeter * 3.25; // converting to feet
+        int first = (int) feet; // converting to int
+
+        double decimal = feet - first;//finds the number after decimal point
+
+        int inches = (int) (12 * decimal);// convert the number after decimal point to inches
+
+        // System.out.print(first + " feet " + inches + " inches");
+
+
+        if (inches < 1) {
+            System.out.print("You will need " + area + " meters squared of glass and " + first + " feet of wood. ");
+
+        } else {
+
+            System.out.print("You will need " + area + " meters squared of glass and " + first + " feet " + inches + " inches of wood. ");
+
+        }
+    }
+    }
+
+
+
+
+
+
+
+
+
